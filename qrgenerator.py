1b1fb4f5ebf3ae62815e2d781112924ad340bc1f
Th3W1k1 <thewiki124@gmail.com>
2025-01-29 11:46:26 -0500

    Create qrgenerator.py
    0.0.1v

---
+++ qrgenerator.py
@@ -0,0 +1,15 @@
+import qrcode
+import uuid
+#datos a codificar
+web=input("introduzca el enlace de su pagina web: ")
+#Generar un UUID
+codigo_unico=uuid.uuid4()
+#crear el generador de codigo QR
+qr=qrcode.QRCode(   version=1,   error_correction=qrcode.constants.ERROR_CORRECT_L,   box_size=10, border=4,)
+#Agregar el UUID al codigo QR
+qr.add_data(codigo_unico)
+qr.make(fit=True)
+#crear la imagen del codugo QR
+img=qr.make_image(fill='black',    back_color='white')
+#guardar la imagen con un nombre unico
+img.save(f"codigo_qr{codigo_unico}.png")
